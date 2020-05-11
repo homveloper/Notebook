0fb30b7ae1d1b40fd19ab28bf2251199c4c344be
totomo <bum4496@naver.com>
2020-05-11 23:35:13 +0900

    ㅇ

--- Web Programming/과제/03 JavaScript/function.js
+++ Web Programming/과제/03 JavaScript/function.js
@@ -32,7 +32,6 @@ function showSlides(n) {
 }
 
 var itemsIndex = [];
-var itemsList = [];
 var itemsPrice = [39800,49800,59800,69800,89800,99800,59800,29800,10800,24800,49800,12800,15800];
 
 function itemSelect(){
@@ -53,6 +52,8 @@ function itemSelect(){
         var order_area = document.getElementById("order_area");
     
         var li = document.createElement("li");
+        li.value = String(selectedItemIndex)
+        li.className = "item"
         var cell = document.createElement("div");
     
         var name = document.createElement("label");
@@ -72,11 +73,11 @@ function itemSelect(){
         var minus = document.createElement("button");
         minus.className = "mius";
         minus.appendChild(document.createTextNode("-"))
-    
+
         var del = document.createElement("button");
         del.className = "del";
         del.appendChild(document.createTextNode("x"));
-    
+
         var price_span = document.createElement("span");
         price_span.className = "price_span";
     
@@ -96,26 +97,27 @@ function itemSelect(){
         cell.appendChild(price_won);
         li.appendChild(cell);
 
-        itemsList.push(li);
-    
         order_area.appendChild(li);
         itemSelect.options[0].selected = true;
         
         plus.addEventListener('click', function(){
             input.value = Number(input.value) + 1
+            setTotalPrice()
         })
 
         minus.addEventListener('click', function(){
             if(Number(input.value) > 1)
                 input.value = Number(input.value) - 1
+            setTotalPrice()
         })
 
         del.addEventListener('click', function(){
+            itemsIndex[li.value] = undefined
             li.parentNode.removeChild(li)
-
-            itemsList.remove
+            setTotalPrice()
         })
 
+        setTotalPrice()
         var acc = document.getElementsByClassName("accordion")[2];
         var panel = acc.nextElementSibling;
         panel.style.maxHeight = panel.scrollHeight + "px";
@@ -123,8 +125,15 @@ function itemSelect(){
 }
 
 function setTotalPrice(){
-    var price = document.getElementsByClassName("price")
-    price.appendChild()
+    var price = document.getElementById("price")
+    var items = document.getElementsByClassName("item")
+
+    var sum = 0 
+    for(var i=0; i<items.length; i++){
+        var input = items[i].firstChild.childNodes[1]
+        sum += input.value * itemsPrice[items.item(i).value -1]
+    }
+    price.innerHTML = sum
 }
 
 function plus(){
